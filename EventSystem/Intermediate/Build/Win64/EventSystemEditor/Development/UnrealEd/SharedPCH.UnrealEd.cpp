bce0dfda23988f633b936776218488bc713ebcdb
pasotee <oalexprea@yahoo.com>
2018-08-25 16:34:41 +0300

    First working copy

--- EventSystem/Intermediate/Build/Win64/EventSystemEditor/Development/UnrealEd/SharedPCH.UnrealEd.cpp
+++
@@ -1,1 +0,0 @@
-#include "E:/Projects/eventsystemunreal/EventSystem/Intermediate/Build/Win64/EventSystemEditor/Development/UnrealEd/SharedPCH.UnrealEd.h"
---
+++ Source/EventSystem/ObsEvent.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ObsEvent.generated.h"
+
+class UObsListener;
+
+/**
+ * Event holding all the listeners and delegating the call to them.
+ */
+UCLASS()
+class EVENTSYSTEM_API UObsEvent : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+	// Invoke the event.
+	UFUNCTION(BlueprintCallable)
+	static void Invoke(UObsEvent* eventToInvoke);
+
+	// Register the listener to the event.
+	void RegisterListener(UObsListener* listener);
+
+	// Unregister the listener from the event.
+	void UnRegisterListener(UObsListener* listener);
+	
+protected:
+	// Listeners registered.
+	TArray<UObsListener*> listeners;
+
+	// Delegate the call to the listeners.
+	void CallListeners();
+};
