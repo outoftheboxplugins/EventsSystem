5a85cf5bc28cf98ee5f39ecca1ec5f0a8cb72d07
pasotee <oalexprea@yahoo.com>
2019-05-25 20:13:09 +0300

    Added editor widget

--- Source/ObsEvent/ObsEvent.Build.cs
+++ Source/ObsEvent/ObsEvent.Build.cs
@@ -14,6 +14,7 @@ namespace UnrealBuildTool.Rules
                     "CoreUObject",
                     "Engine",
                     "UMG",
+                    "EditorWidgets",
                     "UnrealEd",
                     "BlueprintGraph",
                     "KismetCompiler",
