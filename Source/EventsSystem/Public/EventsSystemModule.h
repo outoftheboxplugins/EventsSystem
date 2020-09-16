e43c23649d40ff4457cefcaa46847c280ad5f803
pasotee <pasotee.alexandru@gmail.com>
2020-09-16 20:12:40 +0300

    Events System module

--- Source/EventsSystem/Public/EventsSystemModule.h
+++ Source/EventsSystem/Public/EventsSystemModule.h
@@ -1,27 +1,18 @@
-// Copyright Out-of-the-Box Plugins 2018-2019. All Rights Reserved.
+// Copyright Out-of-the-Box Plugins 2018-2020. All Rights Reserved.
 
 #pragma once
 
 #include "Modules/ModuleInterface.h"
-#include "Modules/ModuleManager.h"
 
 EVENTSSYSTEM_API DECLARE_LOG_CATEGORY_EXTERN(LogEventsSystem, Log, All);
 
 /**
- * Implements the EventsSystem module.
+ * Implements the EventsSystem runtime module.
  */
-class FEventsSystemModule
-	: public IModuleInterface
+
+class FEventsSystemModule : public IModuleInterface
 {
+// IModuleInterface interface
 public:
-
-	//~ IModuleInterface interface
-
-	virtual void StartupModule() override { }
-	virtual void ShutdownModule() override { }
-
-	virtual bool SupportsDynamicReloading() override
-	{
-		return true;
-	}
+	virtual bool SupportsDynamicReloading() override { return true; }
 };
